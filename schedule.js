3bd46cc10d279757589860c909c5d54f6b10fda0
Ari Stassinopoulos <stassinopoulosari@gmail.com>
2026-03-20 21:38:08 -0700

    Add logistics, contact sections. Start on schedule highlighting

---
+++ schedule.js
@@ -0,0 +1,6 @@
+(() => {
+	const dateOfEvent = new Date("2026.04.19 00:00:00 PDT"),
+		dateCurrent = new Date(),
+		offset = dateCurrent.getTime() - dateOfEvent.getTime();
+	// if(offset < 0 || offset > )
+})();
